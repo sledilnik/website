0bee771dd2c3170455ccc0298dad8b14e4900be1
Travis CI <travis@travis-ci.org>
2020-03-21 20:09:49 +0100

    wip

--- src/pages/MapPage.vue
+++ src/pages/MapPage.vue
@@ -1,6 +1,7 @@
 <template>
   <b-container>
     <b-row>
+      <h1>Stanje okužb s COVID-19 po občinah</h1>
       <iframe src="https://covid19.alpaka.si/zemljevid/embedded.html" scrolling="no" width="1140px;" height="800px;" />
     </b-row>
   </b-container>
