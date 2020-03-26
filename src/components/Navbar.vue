dcd25ca6464939f258f4beb6774ecd07a1fd4294
Andraz Vrhovec <andraz@koofr.net>
2020-03-26 15:16:03 +0100

    align nav on right

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,7 +6,7 @@
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
+        <b-navbar-nav class="ml-auto">
           <b-nav-item to="about">O projektu</b-nav-item>
           <b-nav-item to="data">Podatki</b-nav-item>
           <b-nav-item to="stats">Statistika</b-nav-item>
