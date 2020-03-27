d1d0edd446b6494a18c4036545d45cbcf1805e69
Andraz Vrhovec <andraz@koofr.net>
2020-03-27 15:55:26 +0100

    rename navbar entries, closes #64

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,9 +5,9 @@
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
-          <b-nav-item to="stats">Statistika</b-nav-item>
+          <b-nav-item to="stats">Domov</b-nav-item>
           <b-nav-item to="map">Zemljevid</b-nav-item>
-          <b-nav-item to="data">Podatki</b-nav-item>
+          <b-nav-item to="data">Tabela</b-nav-item>
           <b-nav-item to="about">O projektu</b-nav-item>
           <b-nav-item to="sources">Viri</b-nav-item>
           <b-nav-item to="links">Povezave</b-nav-item>
