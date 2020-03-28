62dc9741122cbd8381c252eb6f76a2f249dac7ac
mitjap <mitja.potocin@innovatif.com>
2020-03-28 22:05:42 +0100

    Added active link underline, smaller nav on scroll

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="navbar-container">
-    <b-navbar fixed="top" toggleable="lg">
+    <b-navbar fixed="top" toggleable="lg" :class="{scrolled: scrollPosition > 80}">
       <b-navbar-brand to="stats"></b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
@@ -23,6 +23,19 @@ export default {
   name: "Navbar",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      scrollPosition: ""
+    };
+  },
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      this.scrollPosition = window.scrollY;
+    }
   }
 };
 </script>
@@ -34,6 +47,12 @@ export default {
 }
 nav.navbar {
   background: $yellow;
+  transition: all 0.35s ease-out;
+
+  &.scrolled {
+    padding-top: 0;
+    padding-bottom: 0;
+  }
 
   &.show {
     background: $yellow !important;
@@ -44,14 +63,31 @@ nav.navbar {
   }
 
   .navbar-brand {
-    background: url('../assets/svg/covid-19.svg') no-repeat;
+    background: url("../assets/svg/covid-19.svg") no-repeat;
     width: 145px;
     height: 40px;
-    margin: 12px 0;
+    margin: 10px 0;
   }
 
   a.nav-link {
+    position: relative;
+    display: inline-block;
     color: #000 !important;
+    font-weight: 600;
+
+    &.router-link-active:after {
+      content: "";
+      position: absolute;
+      display: block;
+      left: 0;
+      right: 0;
+      border-bottom: 3px solid rgba(0, 0, 0, 0.75);
+
+      @media (min-width: 992px) {
+        left: 0.5rem;
+        right: 0.5rem;
+      }
+    }
   }
 }
 </style>
