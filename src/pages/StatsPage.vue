0d938082d637f21a3beabcdd61b0add296761d0b
Joh Dokler <joh.dokler@gmail.com>
2020-03-29 23:56:21 +0200

    Translate "Loading" label

--- src/pages/StatsPage.vue
+++ src/pages/StatsPage.vue
@@ -17,7 +17,7 @@
     <b-row>
       <b-col>
         <div class="d-flex justify-content-center mb-3">
-          <b-spinner label="Loading..."></b-spinner>
+          <b-spinner label="Nalagam podatke..."></b-spinner>
         </div>
       </b-col>
     </b-row>
