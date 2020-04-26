c1aab93be81eedd99532b8d61af7e24d57db6265
Luka Renko <46861689+lukarenko@users.noreply.github.com>
2020-04-26 09:08:15 +0200

    move addContainmentMeasuresFlags to common code
    add to log chart

--- src/visualizations/Highcharts.fs
+++ src/visualizations/Highcharts.fs
@@ -80,6 +80,36 @@ let myLoadEvent(name: String) =
         document.dispatchEvent(evt)
     ret
 
+let addContainmentMeasuresFlags(startTime: JsTimestamp) =
+    let events = [|
+    // day, mo, color,    title,       tooltip text
+        4,  3, "#FFFFFF", "1. primer", "Prvi potrjen primer:<br/>turist iz Maroka"
+        6,  3, "#FFe6e6", "DSO",       "Prepoved obiskov v domovih starejših občanov,<br/>potrjena okužba zdravnika v Metliki"
+        8,  3, "#FFFFFF", "Točke",     "16 vstopnih točk za testiranje"
+        10, 3, "#FFe6e6", "Meje",      "Zapora nekaterih mejnih prehodov z Italijo,<br/>poostren nadzor za osebna vozila"
+        13, 3, "#FFFFFF", "Vlada",     "Sprejeta nova vlada"
+        14, 3, "#FFe6e6", "Prevozi",   "Ukinitev javnih prevozov"
+        16, 3, "#FFe6e6", "Šole",      "Zaprtje šol, restavracij"
+        20, 3, "#FFe6e6", "Zbiranje",  "Prepoved zbiranja na javnih mestih"
+        30, 3, "#FFe6e6", "Občine",    "Prepoved gibanja izven meja občin, obvezno razkuževanje <br/>večstanovanjskih zgradb, trgovine za ranljive skupine do 10h"
+        4,  4, "#e6f0ff", "Trgovine",  "Trgovine od 8-10 ure in zadnjo uro izključno <br/>za ranljive skupine (invalidi, upokojenci, nosečnice)"
+        12, 4, "#FFe6e6", "Karantena", "Obvezna 7 dnevna karantena pri prihodu iz tujine"
+        18, 4, "#ebfaeb", "Vikendi",   "Splošna prepoved gibanja in sproščanje omejitev <br/>gibanja med občinami (vikendi...)"
+        20, 4, "#ebfaeb", "Servisi",   "Sproščanje nekaterih dejavnosti (gradnja, servisi, šport na prostem...), <br/>dovoljeni nekateri linijski prevozi"
+    |]
+    {|
+        ``type`` = "flags"
+        shape = "flag"
+        showInLegend = false
+        color = "#444"
+        data =
+            events |> Array.choose (fun (d,m,color,title,text) ->
+                let ts = DateTime(2020,m,d) |> jsTime
+                if ts >= startTime then Some {| x=ts; fillColor=color; title=title; text=text |}
+                else None
+            )
+    |}
+
 let basicChartOptions (scaleType:ScaleType) (className:string)=
     {|
         chart = pojo
@@ -116,7 +146,7 @@ let basicChartOptions (scaleType:ScaleType) (className:string)=
                     {| ``from``=jsTime <| DateTime(2020,3,13);
                        ``to``=jsTime <| DateTime(2020,3,20);
                        color="transparent"
-                       label=Some {| align="center"; text=" Faza 2" |}
+                       label=Some {| align="center"; text="Faza 2" |}
                     |}
                     {| ``from``=jsTime <| DateTime(2020,3,20);
                        ``to``=jsTime <| DateTime(2020,4,8);
