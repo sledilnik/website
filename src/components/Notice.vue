98c16ea4f75e2f72c73475f8741af422d40cf7ab
Mia Erbus <mia.erbus@gmail.com>
2020-05-12 17:15:21 +0200

    add notice banner

---
+++ src/components/Notice.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div class="notice">
+      <span>
+        <b>Zadnja novica: </b>
+        Največji posvet znanstvenikov: ‘Združiti je treba moč institucij in
+        znanstvene skupnosti v boju proti COVID-19’
+      </span>
+      <a href="#" class="notice-button">
+        <img src="../assets/svg/go-to.svg" alt="Go to news" />
+      </a>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  padding-left: 0;
+  padding-right: 0;
+
+  @media only screen and (min-width: 768px) {
+    padding-left: 15px;
+    padding-right: 15px;
+  }
+}
+
+.notice {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: rgba($yellow, 0.1);
+  padding: 20px 32px;
+  margin: 0px auto 48px;
+  width: 100%;
+  cursor: pointer;
+  font-size: 14px;
+  box-shadow: 0 6px 38px -18px rgba(0, 0, 0, 0.3),
+    0 11px 12px -12px rgba(0, 0, 0, 0.22);
+
+  &-button {
+    margin-left: 16px;
+  }
+}
+</style>
