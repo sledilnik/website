4d2f26d91bdb619dc935436b3542708a2f43d59e
Stefan Baebler <stefan.baebler@gmail.com>
2020-05-21 00:36:05 +0200

    infocard: show deceased symbol also in english

--- src/components/cards/InfoCard.vue
+++ src/components/cards/InfoCard.vue
@@ -57,7 +57,7 @@ export default {
       }
     },
     iconClass() {
-      if (this.title == "Umrli") {
+      if (this.title == "Umrli" || this.title == "Confirmed Deaths") { // TODO: make it less fragile
         return "deceased";
       } else if (this.renderValues.lastDay.diff == 0) {
         return 'none';
