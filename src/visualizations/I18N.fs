9be0d435b7f2b673659f2d69e67dd8f89582f667
Luka Renko <46861689+lukarenko@users.noreply.github.com>
2020-05-22 13:56:28 +0200

    Revert "Add options parameter to i18n t"
    This reverts commit 8d7939057bd8ce8e5204e41a1bf0a2623b32e9d9.

--- src/visualizations/I18N.fs
+++ src/visualizations/I18N.fs
@@ -4,13 +4,11 @@ open Fable.Core.JsInterop
 
 type Ii18n =
     abstract t : string -> string
-    abstract t : string * obj -> string
 
 let i18n : Ii18n = importDefault "i18n"
 
 let t key = i18n.t key
 
-let tOptions key options = i18n.t (key, options)
-
-let tt section key =
-    t section + "." + key
+let tt section id = 
+    let key = section + "." + id
+    t key
