4b24c2167ca560435fe6841daf2e1b2858863ed1
Mia Erbus <mia.erbus@gmail.com>
2020-05-26 14:33:39 +0200

    add deceased to hospitilized and ICU

--- src/components/cards/InfoCard.vue
+++ src/components/cards/InfoCard.vue
@@ -4,31 +4,71 @@
       <span class="card-title">{{ title }}</span>
       <span class="card-number">
         {{ renderValues.lastDay.value }}
-        <span class="card-percentage-diff" :class="diffClass">{{ renderValues.lastDay.percentDiff | prefixDiff }}%</span>
+        <span class="card-percentage-diff" :class="diffClass"
+          >{{ renderValues.lastDay.percentDiff | prefixDiff }}%
+        </span>
       </span>
       <div :id="elementId" class="card-diff">
         <span v-if="showAbsolute">
           <div class="trend-icon" :class="[diffClass, iconClass]"></div>
-          <span :class="diffClass">{{ Math.abs(renderValues.lastDay.diff) }}</span>
+          <span :class="diffClass">
+            {{ Math.abs(renderValues.lastDay.diff) }}
+          </span>
         </span>
         <span v-if="showIn">
-          <div class="trend-icon" :class="[diffInOutClass(totalIn, 'down'), iconInOutClass(totalIn, 'down')]"></div>
-          <span class="in" :class="diffInOutClass(totalIn, 'down')">{{ renderInOutValues(totalIn) }}</span>
+          <div
+            class="trend-icon in"
+            :class="[
+              diffTotalClass(totalIn, 'down'),
+              iconTotalClass(totalIn, 'down'),
+            ]"
+          ></div>
+          <span class="in" :class="diffTotalClass(totalIn, 'down')">{{
+            renderTotalValues(totalIn)
+          }}</span>
         </span>
         <span v-if="showOut">
-          <div class="trend-icon" :class="[diffInOutClass(totalOut, 'up'), iconInOutClass(totalOut, 'up')]"></div>
-          <span class="out" :class="diffInOutClass(totalOut, 'up')">{{ renderInOutValues(totalOut) }}</span>
+          <div
+            class="trend-icon out"
+            :class="[
+              diffTotalClass(totalOut, 'up'),
+              iconTotalClass(totalOut, 'up'),
+            ]"
+          ></div>
+          <span class="out" :class="diffTotalClass(totalOut, 'up')">{{
+            renderTotalValues(totalOut)
+          }}</span>
         </span>
-
-        <!-- TODO: x.dayBefore values aren't calculated correctly, check stats.store.js why not -->
-        <!-- <b-tooltip :target="elementId" triggers="hover">
-          {{ $t("infocard.accordingTo", { date: new Date(renderValues.dayBefore.date) }) }}: {{ renderValues.dayBefore.value }}
+        <span v-if="showDeceased">
+          <div
+            class="trend-icon deceased"
+            :class="[
+              diffTotalClass(totalDeceased),
+              iconTotalClass(totalDeceased),
+            ]"
+          ></div>
+          <span class="deceased" :class="diffTotalClass(totalDeceased)">{{
+            renderTotalValues(totalDeceased)
+          }}</span>
+        </span>
+        <b-tooltip :target="elementId" triggers="hover">
+          {{
+            $t('infocard.accordingTo', {
+              date: new Date(renderValues.dayBefore.date),
+            })
+          }}: {{ renderValues.dayBefore.value }}
           <span v-if="renderValues.dayBefore.diff">
             [{{ renderValues.dayBefore.diff | prefixDiff }}]
           </span>
-        </b-tooltip> -->
+        </b-tooltip>
       </div>
-      <div class="data-time">{{ $t("infocard.lastUpdated", { date: new Date(renderValues.lastDay.displayDate) }) }}</div>
+      <div class="data-time">
+        {{
+          $t('infocard.lastUpdated', {
+            date: new Date(renderValues.lastDay.displayDate),
+          })
+        }}
+      </div>
     </div>
     <div class="hp-card" v-else>
       <span class="card-title">{{ title }}</span>
@@ -37,7 +77,7 @@
   </div>
 </template>
 <script>
-import { mapGetters, mapState } from 'vuex';
+import { mapGetters, mapState } from 'vuex'
 
 export default {
   props: {
@@ -45,19 +85,20 @@ export default {
     field: String,
     totalIn: String,
     totalOut: String,
+    totalDeceased: String,
     goodTrend: {
       type: String,
       default: 'down',
     },
     seriesType: {
       type: String,
-      default: 'cum'
-    }
+      default: 'cum',
+    },
   },
   data() {
     return {
       show: false,
-    };
+    }
   },
   computed: {
     ...mapGetters('stats', ['lastChange']),
@@ -65,28 +106,28 @@ export default {
     ...mapState('stats', ['exportTime', 'loaded']),
     diffClass() {
       if (this.renderValues.lastDay.diff === 0) {
-        return 'no-change';
+        return 'no-change'
       } else if (this.renderValues.lastDay.diff > 0) {
-        return this.goodTrend === 'down' ? 'bad' : 'good';
+        return this.goodTrend === 'down' ? 'bad' : 'good'
       } else {
-        return this.goodTrend === 'down' ? 'good' : 'bad';
+        return this.goodTrend === 'down' ? 'good' : 'bad'
       }
     },
     iconClass() {
       if (this.field === 'statePerTreatment.deceasedToDate') {
-        return "deceased";
-      } else if (this.renderValues.lastDay.diff === 0) {
-        return 'none';
+        return 'deceased'
+      }
+      if (this.renderValues.lastDay.diff === 0) {
+        return 'none'
       } else if (this.renderValues.lastDay.diff > 0) {
-        return "up";
+        return 'up'
       } else {
-        return "down";
+        return 'down'
       }
     },
     renderValues() {
-      const x = this.lastChange(this.field, this.seriesType == 'cum');
+      const x = this.lastChange(this.field, this.seriesType == 'cum')
       if (x) {
-        // TODO: x.dayBefore values aren't calculated correctly, check stats.store.js why not
         if (this.seriesType == 'cum') {
           x.lastDay.displayDate = x.lastDay.firstDate || x.lastDay.date
           x.dayBefore.displayDate = x.dayBefore.firstDate || x.dayBefore.date
@@ -98,46 +139,71 @@ export default {
       return x
     },
     elementId() {
-      return this.field;
+      return this.field
     },
     showAbsolute() {
-      return !this.totalIn && !this.totalOut || (this.renderInOutValues(this.totalIn) === this.renderInOutValues(this.totalOut))
+      return (
+        (!this.totalIn && !this.totalOut && !this.totalDeceased) ||
+        this.renderTotalValues(this.totalIn) ===
+          this.renderTotalValues(this.totalOut)
+      )
     },
     showIn() {
-      return this.totalIn && (this.renderInOutValues(this.totalIn) !== this.renderInOutValues(this.totalOut))
+      return (
+        this.totalIn &&
+        this.renderTotalValues(this.totalIn) !==
+          this.renderTotalValues(this.totalOut) &&
+        this.renderTotalValues(this.totalIn) > 0
+      )
     },
     showOut() {
-      return this.totalOut && (this.renderInOutValues(this.totalIn) !== this.renderInOutValues(this.totalOut))
-    }
+      return (
+        this.totalOut &&
+        this.renderTotalValues(this.totalIn) !==
+          this.renderTotalValues(this.totalOut) &&
+        this.renderTotalValues(this.totalOut) > 0
+      )
+    },
+    showDeceased() {
+      return this.totalDeceased
+    },
   },
   methods: {
-    renderInOutValues(inOut) {
-      const lastDay = this.patients.filter(x => {
-        return x.day === this.renderValues.lastDay.date.getDate() &&
-               x.month === this.renderValues.lastDay.date.getMonth() + 1 &&
-               x.year === this.renderValues.lastDay.date.getFullYear()
+    renderTotalValues(value) {
+      const lastDay = this.patients.filter((x) => {
+        return (
+          x.day === this.renderValues.lastDay.date.getDate() &&
+          x.month === this.renderValues.lastDay.date.getMonth() + 1 &&
+          x.year === this.renderValues.lastDay.date.getFullYear()
+        )
       })
-      return _.get(lastDay[0], inOut)
+      return _.get(lastDay[0], value)
     },
-    diffInOutClass(inOut, goodTrend) {
-      if (this.renderInOutValues(inOut) === 0) {
-        return 'no-change';
+    diffTotalClass(value, goodTrend) {
+      if (this.totalDeceased > 0) {
+        return
+      }
+      if (this.renderTotalValues(value) === 0) {
+        return 'no-change'
       } else {
-        return this.goodTrend === goodTrend ? 'bad' : 'good';
+        return this.goodTrend === goodTrend ? 'bad' : 'good'
       }
     },
-    iconInOutClass(inOut, goodTrend) {
-      if (this.renderInOutValues(inOut) === 0) {
-        return 'none';
+    iconTotalClass(value, goodTrend) {
+      if (this.field === 'statePerTreatment.deceasedToDate') {
+        // console.log('here', this.field, this.totalDeceased)
+        return 'deceased'
+      } else if (this.renderTotalValues(value) === 0) {
+        return 'none'
       } else {
-        return this.goodTrend === goodTrend ? 'up' : 'down';
+        return this.goodTrend === goodTrend ? 'up' : 'down'
       }
     },
   },
   mounted() {
-    this.show = true;
+    this.show = true
   },
-};
+}
 </script>
 
 <style scoped lang="scss">
@@ -209,7 +275,7 @@ export default {
   }
 
   .trend-icon {
-    display:inline-block;
+    display: inline-block;
     width: 24px;
     height: 24px;
     object-fit: contain;
@@ -234,7 +300,8 @@ export default {
     }
 
     &.deceased {
-      -webkit-mask: url(../../assets/svg/close-circle-deceased.svg) no-repeat center;
+      -webkit-mask: url(../../assets/svg/close-circle-deceased.svg) no-repeat
+        center;
       mask: url(../../assets/svg/close-circle-deceased.svg) no-repeat center;
       background-color: #404040;
     }
@@ -257,7 +324,7 @@ export default {
   color: #20b16d;
 }
 
-.no-change {
+.no-change, .deceased {
   color: #a0a0a0;
 }
 
--- src/pages/StatsPage.vue
+++ src/pages/StatsPage.vue
@@ -8,11 +8,38 @@
         </b-col>
       </b-row>
       <div class="cards-wrapper latest-data-boxes">
-        <Info-card :title="$t('infocard.confirmedToDate')" field="cases.confirmedToDate" series-type="state" />
-        <Info-card :title="$t('infocard.recoveredToDate')" field="cases.recoveredToDate" good-trend="up" series-type="state" />
-        <Info-card :title="$t('infocard.inHospital')" field="statePerTreatment.inHospital" totalIn="total.inHospital.in" totalOut="total.inHospital.out" series-type="state" />
-        <Info-card :title="$t('infocard.icu')" field="statePerTreatment.inICU" totalIn="total.icu.in" totalOut="total.icu.out" series-type="state" />
-        <Info-card :title="$t('infocard.deceasedToDate')" field="statePerTreatment.deceasedToDate" series-type="state" />
+        <Info-card
+          :title="$t('infocard.confirmedToDate')"
+          field="cases.confirmedToDate"
+          series-type="state"
+        />
+        <Info-card
+          :title="$t('infocard.recoveredToDate')"
+          field="cases.recoveredToDate"
+          good-trend="up"
+          series-type="state"
+        />
+        <Info-card
+          :title="$t('infocard.inHospital')"
+          field="statePerTreatment.inHospital"
+          totalIn="total.inHospital.in"
+          totalOut="total.inHospital.out"
+          totalDeceased="total.deceased.hospital.today"
+          series-type="state"
+        />
+        <Info-card
+          :title="$t('infocard.icu')"
+          field="statePerTreatment.inICU"
+          totalIn="total.icu.in"
+          totalOut="total.icu.out"
+          totalDeceased="total.deceased.hospital.icu.today"
+          series-type="state"
+        />
+        <Info-card
+          :title="$t('infocard.deceasedToDate')"
+          field="statePerTreatment.deceasedToDate"
+          series-type="state"
+        />
       </div>
       <b-row cols="12">
         <b-col>
@@ -25,14 +52,14 @@
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapState } from 'vuex'
 
 // import Loader from 'components/Loader';
-import InfoCard from 'components/cards/InfoCard';
-import TimeStamp from 'components/TimeStamp';
-import Notice from 'components/Notice';
+import InfoCard from 'components/cards/InfoCard'
+import TimeStamp from 'components/TimeStamp'
+import Notice from 'components/Notice'
 
-import { Visualizations } from 'visualizations/App.fsproj';
+import { Visualizations } from 'visualizations/App.fsproj'
 
 export default {
   name: 'StatsPage',
@@ -49,7 +76,7 @@ export default {
   data() {
     return {
       loaded: false,
-    };
+    }
   },
   computed: {
     ...mapState('stats', {
@@ -62,20 +89,20 @@ export default {
   mounted() {
     this.$nextTick(() => {
       // must use next tick, so whole DOM is ready and div#id=visualizations exists
-      Visualizations('visualizations', this.$route.query);
-    });
+      Visualizations('visualizations', this.$route.query)
+    })
 
     // stupid spinner impl, but i do not know better (charts are react component, no clue when they are rendered)
     let checker = setInterval(() => {
-      let elm = document.querySelector('.highcharts-point');
+      let elm = document.querySelector('.highcharts-point')
       if (elm) {
-        document.querySelector('.stats-page').classList.add('loaded');
-        this.loaded = true;
-        clearInterval(checker);
+        document.querySelector('.stats-page').classList.add('loaded')
+        this.loaded = true
+        clearInterval(checker)
       }
-    }, 80);
+    }, 80)
   },
-};
+}
 </script>
 
 <style lang="sass"></style>
