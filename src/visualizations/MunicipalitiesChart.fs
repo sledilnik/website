b7245af1db7bbdea3080e596a1fd9d300962f8fd
Luka Renko <46861689+lukarenko@users.noreply.github.com>
2020-06-25 11:00:07 +0200

    quick hack for new

--- src/visualizations/MunicipalitiesChart.fs
+++ src/visualizations/MunicipalitiesChart.fs
@@ -318,12 +318,17 @@ let renderMunicipality (state : State) (municipality : Municipality) =
                                 prop.className "active"
                                 prop.text (sprintf "%d" (municipality.ActiveCases |> Option.defaultValue 0)) ]
                             Html.div [
-                                prop.className "total"
-                                prop.text (sprintf "%d" (municipality.MaxConfirmedCases |> Option.defaultValue 0)) ]
-                            if municipality.NewCases.IsSome then// TODO: put it in red and to the right of total    
-                                Html.div [
-                                    prop.className "new"
-                                    prop.text (sprintf "+%d" (municipality.NewCases |> Option.defaultValue 0)) ]
+                                prop.className "total-and-new"
+                                prop.children [
+                                    Html.div [
+                                        prop.className "total"
+                                        prop.text (sprintf "%d" (municipality.MaxConfirmedCases |> Option.defaultValue 0)) ]
+                                    if municipality.NewCases.IsSome then // TODO: put it in red and to the right of total    
+                                        Html.div [
+                                            prop.className "new" 
+                                            prop.text (sprintf " +%d" (municipality.NewCases |> Option.defaultValue 0)) ] 
+                                ]
+                            ]
                             Html.div [
                                 prop.className "date"
                                 prop.text (I18N.tOptions "days.date" {| date = municipality.LastConfirmedCase.Date |})]
