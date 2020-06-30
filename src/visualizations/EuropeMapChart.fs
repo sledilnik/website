0e636317cd573d90000bbc84c2d979e647db274a
Luka Renko <46861689+lukarenko@users.noreply.github.com>
2020-06-30 14:46:59 +0200

    convert OWD date of data

--- src/visualizations/EuropeMapChart.fs
+++ src/visualizations/EuropeMapChart.fs
@@ -107,7 +107,7 @@ let calculateOwdIncidence (data : Data.OurWorldInData.DataPoint list) =
 
 let renderIncidenceMap state geoJson owdData =
 
-    let owdDate = getOwdDate owdData
+    let owdDate = DateTime.Parse (getOwdDate owdData)
     let owdIncidence = calculateOwdIncidence owdData
 
     let pointFormat =
