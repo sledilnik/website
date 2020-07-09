965d9719b4ce88566b9600d4cfc9bb05526df53e
Stefan Baebler <stefan.baebler@gmail.com>
2020-07-09 14:33:48 +0200

    hide github button in header

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -20,10 +20,12 @@
       <router-link to="team" class="router-link"><span>{{ $t("navbar.team") }}</span></router-link>
       <router-link to="sources" class="router-link"><span>{{ $t("navbar.sources") }}</span></router-link>
       <router-link to="links" class="router-link"><span>{{ $t("navbar.links") }}</span></router-link>
+      <!--
       <a href="https://github.com/sledilnik" target="_blank" class="router-link router-link-icon">
         <img src="../assets/svg/gh-icon.svg" :alt="$t('navbar.github')" />
         <span>{{ $t("navbar.github") }}</span>
       </a>
+      -->
       <div v-if="!isMobile" class="router-link router-link-lang-switcher">
         <div>
           <div class="lang" @click="toggleDropdown">
