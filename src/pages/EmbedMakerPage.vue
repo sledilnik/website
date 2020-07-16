3b4729d0a8c5be56369cf83fca87ef16e7396738
Stefan Baebler <stefan.baebler@gmail.com>
2020-07-16 16:10:16 +0200

    removed todo

--- src/pages/EmbedMakerPage.vue
+++ src/pages/EmbedMakerPage.vue
@@ -40,7 +40,7 @@ export default {
         },
         "MetricsComparison": {
           value: "MetricsComparison",
-          text: this.$t('charts.metricsComparison.title', { context: process.env.VUE_APP_LOCALE_CONTEXT } ), // TODO: add this context by default to all calls
+          text: this.$t('charts.metricsComparison.title', { context: process.env.VUE_APP_LOCALE_CONTEXT } ),
           dimensions: [1140, 780]
         },
         "EuropeMap": {
