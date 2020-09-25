2963c2ae6b6e489486f4ea5c5dd30aabe1e95c65
Luka Renko <46861689+lukarenko@users.noreply.github.com>
2020-09-25 08:46:32 +0200

    each page independant buttons

--- src/visualizations/MetricsComparisonChart.fs
+++ src/visualizations/MetricsComparisonChart.fs
@@ -10,7 +10,7 @@ open Browser
 open Highcharts
 open Types
 
-type XAxisType =
+type MetricType =
     | Active
     | Today
     | ToDate
@@ -41,48 +41,39 @@ type MetricCfg = {
     Color : string
     Visible : bool
     Line : Highcharts.DashStyle
+    Type : MetricType
     Id: string
 }
 
 type Metrics = MetricCfg list
 
 module Metrics  =
-    let metricsActive = [
-        { Metric=ActiveCases;          Color="#dba51d"; Visible=true;  Line=Dash;   Id="activeCases" }
-        { Metric=InHospital;           Color="#be7A2a"; Visible=true;  Line=Solid;  Id="hospitalized" }
-        { Metric=InICU;                Color="#d96756"; Visible=true;  Line=Solid;  Id="icu" }
-        { Metric=OnVentilator;         Color="#bf5747"; Visible=true;  Line=Solid;  Id="ventilator" }
+    let initial = [
+        { Metric=ActiveCases;          Color="#dba51d"; Visible=true;  Line=Solid;  Type=Active;    Id="activeCases" }
+        { Metric=InHospital;           Color="#be7A2a"; Visible=true;  Line=Solid;  Type=Active;    Id="hospitalized" }
+        { Metric=InICU;                Color="#d96756"; Visible=true;  Line=Solid;  Type=Active;    Id="icu" }
+        { Metric=OnVentilator;         Color="#bf5747"; Visible=true;  Line=Solid;  Type=Active;    Id="ventilator" }
+        { Metric=PerformedTests;       Color="#19aebd"; Visible=false; Line=Solid;  Type=Today;     Id="testsPerformed" }
+        { Metric=ConfirmedCasesToday;  Color="#bda506"; Visible=true;  Line=Solid;  Type=Today;     Id="confirmedCases" }
+        { Metric=OutOfHospital;        Color="#20b16d"; Visible=false; Line=Solid;  Type=Today;     Id="hospitalDischarged" }
+        { Metric=Deceased;             Color="#000000"; Visible=true;  Line=Solid;  Type=Today;     Id="deceased" }
+        { Metric=PerformedTestsToDate; Color="#19aebd"; Visible=false; Line=Dot;    Type=ToDate;    Id="testsPerformed" }
+        { Metric=ConfirmedCasesToDate; Color="#bda506"; Visible=true; Line=Dot;     Type=ToDate;    Id="confirmedCases" }
+        { Metric=OutOfHospitalToDate;  Color="#20b16d"; Visible=false; Line=Dot;    Type=ToDate;    Id="hospitalDischarged" }
+        { Metric=DeceasedToDate;       Color="#000000"; Visible=true; Line=Dot;     Type=ToDate;    Id="deceased" }
+        { Metric=RecoveredToDate;      Color="#8cd4b2"; Visible=true; Line=Dot;     Type=ToDate;    Id="recovered" }
     ]
 
-    let metricsToday = [
-        { Metric=PerformedTests;       Color="#19aebd"; Visible=false; Line=Solid;  Id="testsPerformed" }
-        { Metric=ConfirmedCasesToday;  Color="#bda506"; Visible=true;  Line=Solid;  Id="confirmedCases" }
-        { Metric=OutOfHospital;        Color="#20b16d"; Visible=false; Line=Solid;  Id="hospitalDischarged" }
-        { Metric=Deceased;             Color="#000000"; Visible=false; Line=Solid;  Id="deceased" }
-    ]
-
-    let metricsToDate = [
-        { Metric=PerformedTestsToDate; Color="#19aebd"; Visible=false; Line=Dot;    Id="testsPerformed" }
-        { Metric=ConfirmedCasesToDate; Color="#bda506"; Visible=true;  Line=Dot;    Id="confirmedCases" }
-        { Metric=OutOfHospitalToDate;  Color="#20b16d"; Visible=false; Line=Dot;    Id="hospitalDischarged" }
-        { Metric=DeceasedToDate;       Color="#000000"; Visible=false; Line=Dot;    Id="deceased" }
-        { Metric=RecoveredToDate;      Color="#8cd4b2"; Visible=false; Line=Dash;   Id="recovered" }
-    ]
-
-    let byType mType =
-        match mType with
-        | Active -> metricsActive
-        | Today -> metricsToday
-        | ToDate -> metricsToDate
 
     /// Find a metric in the list and apply provided function to modify its value
     let update (fn: MetricCfg -> MetricCfg) metric metrics =
         metrics
         |> List.map (fun mc -> if mc.Metric = metric then fn mc else mc)
+        
 
 type State =
     { ScaleType : ScaleType
-      XAxisType : XAxisType
+      MetricType : MetricType
       Data : StatsData
       Metrics : Metrics
       RangeSelectionButtonIndex: int
@@ -91,14 +82,14 @@ type State =
 type Msg =
     | ToggleMetricVisible of Metric
     | ScaleTypeChanged of ScaleType
-    | XAxisTypeChanged of XAxisType
+    | MetricTypeChanged of MetricType
     | RangeSelectionChanged of int
 
 let init data : State * Cmd<Msg> =
     let state = {
         ScaleType = Linear
-        XAxisType = Active
-        Metrics = Metrics.byType(Active)
+        MetricType = Active
+        Metrics = Metrics.initial
         Data = data
         RangeSelectionButtonIndex = 0
     }
@@ -113,10 +104,10 @@ let update (msg: Msg) (state: State) : State * Cmd<Msg> =
         }, Cmd.none
     | ScaleTypeChanged scaleType ->
         { state with ScaleType = scaleType }, Cmd.none
-    | XAxisTypeChanged xAxisType ->
+    | MetricTypeChanged metricType ->
         { state with 
-            XAxisType = xAxisType
-            Metrics = Metrics.byType(xAxisType) }, Cmd.none
+            MetricType = metricType 
+            }, Cmd.none
     | RangeSelectionChanged buttonIndex ->
         { state with RangeSelectionButtonIndex = buttonIndex }, Cmd.none
 
@@ -144,22 +135,24 @@ let renderChartOptions state dispatch =
     let allSeries = [
         let mutable startTime = DateTime.Today |> jsTime
         for metric in state.Metrics do
-            let pointData = metricDataGenerator metric
-            yield pojo
-                {|
-                    visible = metric.Visible
-                    color = metric.Color
-                    name = I18N.tt "charts.metricsComparison" metric.Id
-                    dashStyle = metric.Line |> DashStyle.toString
-                    data =
-                        state.Data
-                        |> Seq.map (fun dp -> (xAxisPoint dp |> jsTime12h, pointData dp))
-                        |> Seq.skipWhile (fun (ts,value) ->
-                            if metric.Visible && value.IsSome then
-                                startTime <- min startTime ts
-                            value.IsNone)
-                        |> Seq.toArray
-                |}
+            if metric.Type = state.MetricType  
+            then
+                let pointData = metricDataGenerator metric
+                yield pojo
+                    {|
+                        visible = metric.Visible
+                        color = metric.Color
+                        name = I18N.tt "charts.metricsComparison" metric.Id
+                        dashStyle = metric.Line |> DashStyle.toString
+                        data =
+                            state.Data
+                            |> Seq.map (fun dp -> (xAxisPoint dp |> jsTime12h, pointData dp))
+                            |> Seq.skipWhile (fun (ts,value) ->
+                                if metric.Visible && value.IsSome then
+                                    startTime <- min startTime ts
+                                value.IsNone)
+                            |> Seq.toArray
+                    |}
         yield addContainmentMeasuresFlags startTime None |> pojo
     ]
 
@@ -208,34 +201,35 @@ let renderMetricsSelectors state dispatch =
         prop.className "metrics-selectors"
         prop.children [
             for mc in state.Metrics do
-                yield renderMetricSelector mc dispatch
+                if mc.Type = state.MetricType 
+                then yield renderMetricSelector mc dispatch
         ]
     ]
 
-let renderXAxisSelectors (activeXAxisType: XAxisType) dispatch =
-    let renderXAxisSelector (axisSelector: XAxisType) =
-        let active = axisSelector = activeXAxisType
+let renderMetricTypeSelectors (activeMetricType: MetricType) dispatch =
+    let renderMetricTypeSelector (typeSelector: MetricType) =
+        let active = typeSelector = activeMetricType
         Html.div [
-            prop.onClick (fun _ -> dispatch axisSelector)
+            prop.onClick (fun _ -> dispatch typeSelector)
             Utils.classes
                 [(true, "chart-display-property-selector__item")
                  (active, "selected") ]
-            prop.text (axisSelector |> XAxisType.getName)
+            prop.text (typeSelector |> MetricType.getName)
         ]
 
-    let xAxisTypesSelectors =
+    let metricTypesSelectors =
         [ Active; Today; ToDate ]
-        |> List.map renderXAxisSelector
+        |> List.map renderMetricTypeSelector
 
     Html.div [
         prop.className "chart-display-property-selector"
-        prop.children ((Html.text (I18N.t "charts.common.xAxis")) :: xAxisTypesSelectors)
+        prop.children ((Html.text (I18N.t "charts.common.view")) :: metricTypesSelectors)
     ]
 
 let render state dispatch =
     Html.div [
         Utils.renderChartTopControls [
-            renderXAxisSelectors state.XAxisType (XAxisTypeChanged >> dispatch)
+            renderMetricTypeSelectors state.MetricType (MetricTypeChanged >> dispatch)
             Utils.renderScaleSelector state.ScaleType (ScaleTypeChanged >> dispatch)
         ]
         renderChartContainer state dispatch
