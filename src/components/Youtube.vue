64d7fb1a103c8973d5e7943f195bbe06e817b1e4
Mia Erbus <mia.erbus@gmail.com>
2020-09-30 16:35:01 +0200

    animation page

---
+++ src/components/Youtube.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="youtube-embed">
+    <iframe :src="videoId"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Youtube',
+  props: {
+    id: String,
+  },
+  data() {
+    return {
+      videoId: 'https://www.youtube-nocookie.com/embed/' + this.id,
+    }
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.youtube-embed {
+  position: relative;
+  height: 0;
+  overflow: hidden;
+  padding-bottom: 56.25%;
+}
+
+iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border: 0;
+}
+</style>
