0e313073db5ab0c4e152080d5e9f59ccf11f12f4
Mia Erbus <mia.erbus@gmail.com>
2020-10-05 11:59:12 +0200

    Revert "align card rows with subgrid"
    This reverts commit 77b41d6f2cbfc2a8d6e2f89605cd09aee8d5dec7.

--- src/components/cards/InfoCard.vue
+++ src/components/cards/InfoCard.vue
@@ -1,56 +1,58 @@
 <template>
-  <div :title="title" class="hp-card" v-if="loaded">
-    <div class="card-title">{{ title }}</div>
-    <div class="card-number">
-      <span>{{ renderValues.lastDay.value }}</span>
-      <div class="card-percentage-diff" :class="diffClass">
-        {{ renderValues.lastDay.percentDiff | prefixDiff }}%
+  <div :title="title" class="hp-card-holder">
+    <div class="hp-card" v-if="loaded">
+      <div class="card-title">{{ title }}</div>
+      <div class="card-number">
+        <span>{{ renderValues.lastDay.value }}</span>
+        <div class="card-percentage-diff" :class="diffClass">
+          {{ renderValues.lastDay.percentDiff | prefixDiff }}%
+        </div>
       </div>
-    </div>
-    <div :id="field" class="card-diff">
-      <div v-if="showAbsolute">
-        <div class="trend-icon" :class="[diffClass, iconClass]"></div>
-        <span :class="diffClass"
-          >{{ Math.abs(renderValues.lastDay.diff) }}
-        </span>
-      </div>
-      <div v-if="showIn" class="card-diff-item">
-        <div class="trend-icon in bad up"></div>
-        <span v-if="field === 'cases.active'" class="in bad">{{
-          renderActiveValues(fieldNewCases).lastDay.value
-        }}</span>
-        <span v-else class="in bad">{{ renderTotalValues(totalIn) }}</span>
-      </div>
-      <div v-if="showOut" class="card-diff-item">
-        <div class="trend-icon out good down"></div>
-        <span v-if="field === 'cases.active'" class="out good">{{
-          renderActiveValues(fieldNewCases).lastDay.value -
-            renderActiveValues(field).lastDay.diff -
+      <div :id="field" class="card-diff">
+        <div v-if="showAbsolute">
+          <div class="trend-icon" :class="[diffClass, iconClass]"></div>
+          <span :class="diffClass"
+            >{{ Math.abs(renderValues.lastDay.diff) }}
+          </span>
+        </div>
+        <div v-if="showIn" class="card-diff-item">
+          <div class="trend-icon in bad up"></div>
+          <span v-if="field === 'cases.active'" class="in bad">{{
+            renderActiveValues(fieldNewCases).lastDay.value
+          }}</span>
+          <span v-else class="in bad">{{ renderTotalValues(totalIn) }}</span>
+        </div>
+        <div v-if="showOut" class="card-diff-item">
+          <div class="trend-icon out good down"></div>
+          <span v-if="field === 'cases.active'" class="out good">{{
+            renderActiveValues(fieldNewCases).lastDay.value -
+              renderActiveValues(field).lastDay.diff -
+              renderActiveValues(fieldDeceased).lastDay.value
+          }}</span>
+          <span v-else class="out good">{{ renderTotalValues(totalOut) }}</span>
+        </div>
+        <div v-if="showDeceased" class="card-diff-item">
+          <div class="trend-icon deceased"></div>
+          <span v-if="field === 'cases.active'" class="deceased">{{
             renderActiveValues(fieldDeceased).lastDay.value
-        }}</span>
-        <span v-else class="out good">{{ renderTotalValues(totalOut) }}</span>
+          }}</span>
+          <span v-else class="deceased"
+            >{{ renderTotalValues(totalDeceased) }}
+          </span>
+        </div>
       </div>
-      <div v-if="showDeceased" class="card-diff-item">
-        <div class="trend-icon deceased"></div>
-        <span v-if="field === 'cases.active'" class="deceased">{{
-          renderActiveValues(fieldDeceased).lastDay.value
-        }}</span>
-        <span v-else class="deceased"
-          >{{ renderTotalValues(totalDeceased) }}
-        </span>
+      <div class="data-time">
+        {{
+          $t('infocard.lastUpdated', {
+            date: new Date(renderValues.lastDay.displayDate),
+          })
+        }}
       </div>
     </div>
-    <div class="data-time">
-      {{
-        $t('infocard.lastUpdated', {
-          date: new Date(renderValues.lastDay.displayDate),
-        })
-      }}
+    <div class="hp-card" v-else>
+      <div class="card-title">{{ title }}</div>
+      <font-awesome-icon icon="spinner" spin />
     </div>
-  </div>
-  <div class="hp-card" v-else>
-    <div class="card-title">{{ title }}</div>
-    <font-awesome-icon icon="spinner" spin />
   </div>
 </template>
 <script>
@@ -200,14 +202,17 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.hp-card-holder {
+  flex: 1;
+}
+
 .hp-card {
   display: flex;
   flex-direction: column;
-  display: grid;
-  grid-row: span 4;
-  grid-template-rows: auto auto 1fr auto;
-  grid-template-rows: subgrid;
-  grid-row-gap: 0;
+  // display: grid;
+  // grid-template-rows: auto auto 1fr auto; // TODO: fix for other languages (hr,de)
+  min-height: 196px;
+  height: 100%;
   padding: 18px;
   background: #fff;
   box-shadow: $element-box-shadow;
@@ -228,14 +233,12 @@ export default {
 .card-title {
   font-size: 14px;
   font-weight: 700;
-  grid-column-end: span 2;
 }
 
 .card-number {
   font-size: 32px;
   font-weight: 700;
   white-space: nowrap;
-  grid-column-end: span 2;
 }
 
 .card-percentage-diff {
@@ -248,7 +251,6 @@ export default {
 .card-diff {
   font-size: 14px;
   margin-bottom: 0.7rem;
-  grid-column-end: span 2;
 
   .card-diff-item {
     display: inline-block;
@@ -316,7 +318,6 @@ export default {
 .data-time {
   font-size: 12px;
   color: #a0a0a0;
-  // margin-top: auto;
-  grid-column-end: span 2;
+  margin-top: auto;
 }
 </style>
