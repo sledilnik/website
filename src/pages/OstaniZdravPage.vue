11dd5c33775e69daa1b29957024c154adc306adc
Andraz Vrhovec <andraz@koofr.net>
2020-10-09 13:57:57 +0200

    Integration #ostanizdrav charts into sledilnik - POC

---
+++ src/pages/OstaniZdravPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-container>
+    <b-row class="d-flex justify-content-center">
+      <div id="validVis"></div>
+    </b-row>
+    <b-row class="d-flex justify-content-center">
+      <div id="validByRiskVis"></div>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import embed from "vega-embed";
+
+import validPlot from "@/vega/valid.json";
+import validByRiskPlot from "@/vega/validByRisk.json";
+
+export default {
+  name: "OstaniZdravPage",
+  mounted() {
+    embed("#validVis", validPlot);
+    embed("#validByRiskVis", validByRiskPlot);
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+</style>
