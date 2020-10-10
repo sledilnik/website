2a1e0fad1d2c3c6467ff8f1d4b74e6811354bacb
Matjaž Drolc <mdrolc@gmail.com>
2020-10-10 20:20:24 +0200

    feat: Added additional weekly statistics

--- src/visualizations/WeeklyStatsChart.fs
+++ src/visualizations/WeeklyStatsChart.fs
@@ -1,5 +1,6 @@
 module WeeklyStatsChart
 
+open System.Runtime.InteropServices
 open Elmish
 open Feliz
 open Feliz.ElmishComponents
@@ -8,28 +9,54 @@ open Browser
 open Types
 open Highcharts
 
+type DisplayType =
+    | Quarantine
+    | BySource
+    | BySourceCountry
+  with
+    static member all = [ Quarantine; BySource; BySourceCountry ]
+    static member getName = function
+        | Quarantine     -> I18N.t "charts.weeklyStats.quarantine"
+        | BySource     -> I18N.t "charts.weeklyStats.bySource"
+        | BySourceCountry     -> I18N.t "charts.weeklyStats.bySourceCountry"
 
 type Series =
     | ConfirmedCases
     | SentToQuarantine
+    | ConfirmedFromQuarantine
+    | ImportedCases
+    | ImportRelatedCases
+    | SourceUnknown
+    | LocalSource
 
 module Series =
-    let all = [ SentToQuarantine; ConfirmedCases ]
+    let quarantine = [ SentToQuarantine; ConfirmedCases; ConfirmedFromQuarantine ]
+    let bySource = [ImportedCases; ImportRelatedCases;  LocalSource; SourceUnknown]
 
     let getSeriesInfo =
         function
-        | SentToQuarantine -> true, "#d96756", "sentToQuarantine"
-        | ConfirmedCases -> true, "#d5c768", "confirmedCases"
+        | SentToQuarantine ->  "#cccccc", "sentToQuarantine", 0
+        | ConfirmedFromQuarantine ->  "#6f42c1", "confirmedFromQuarantine", 1
+        | ConfirmedCases ->  "#d5c768", "confirmedCases", 1
+
+        | ImportedCases -> "#007bff", "importedCases", 0
+        | ImportRelatedCases -> "#17a2b8", "importRelatedCases", 0
+        | SourceUnknown -> "#6610f2", "sourceUnknown", 0
+        | LocalSource ->"#ffc107", "localSource", 0
 
 type State =
-    { data: WeeklyStatsData
+    { displayType: DisplayType
+      data: WeeklyStatsData
       RangeSelectionButtonIndex: int }
 
-type Msg = RangeSelectionChanged of int
+type Msg =
+    | RangeSelectionChanged of int
+    | ChangeDisplayType of DisplayType
 
 let init data: State * Cmd<Msg> =
     let state =
-        { data = data
+        { displayType = Quarantine
+          data = data
           RangeSelectionButtonIndex = 0 }
 
     state, Cmd.none
@@ -40,34 +67,62 @@ let update (msg: Msg) (state: State): State * Cmd<Msg> =
         { state with
               RangeSelectionButtonIndex = buttonIndex },
         Cmd.none
+    | ChangeDisplayType displayType ->
+        { state with displayType = displayType },
+        Cmd.none
 
 let tooltipFormatter jsThis =
     let pts: obj [] = jsThis?points
     let fmtWeekYearFromTo = pts.[0]?point?fmtWeekYearFromTo
+    let arrows p = match p?point?seriesId with
+                                   | "confirmedFromQuarantine" -> "↳ "
+                                   |_ -> ""
 
     fmtWeekYearFromTo
     + (pts
        |> Seq.map (fun p ->
-           sprintf """<br><span style="color:%s">●</span> %s: <b>%s</b>""" p?series?color p?series?name p?point?y)
+           sprintf """<br>%s<span style="color:%s">●</span> %s: <b>%s</b>""" (arrows p) p?series?color p?series?name p?point?fmtTotal)
        |> String.concat "<br>")
+
+let splitOutFromTotal (split : int option) (total : int option)  =
+    match split, total with
+    | Some split_, Some total_ -> Some (total_ - split_)
+    | None, Some _ -> total
+    | _ -> None
 
 let renderChartOptions (state: State) dispatch =
     let renderSeries series =
         let getPoint: (WeeklyStatsDataPoint -> int option) =
             match series with
+            | ConfirmedCases -> fun dp -> dp.ConfirmedCases |> splitOutFromTotal dp.Source.FromQuarantine
+            | SentToQuarantine -> fun dp -> dp.SentToQuarantine
+            | ConfirmedFromQuarantine -> fun dp -> dp.Source.FromQuarantine
+            | ImportedCases -> fun dp -> dp.Source.Import
+            | ImportRelatedCases -> fun dp -> dp.Source.ImportRelated
+            | SourceUnknown -> fun dp -> dp.Source.Unknown
+            | LocalSource -> fun dp -> dp.Source.Local
+
+        let getPointTotal: (WeeklyStatsDataPoint -> int option) =
+            match series with
             | ConfirmedCases -> fun dp -> dp.ConfirmedCases
             | SentToQuarantine -> fun dp -> dp.SentToQuarantine
+            | ConfirmedFromQuarantine -> fun dp -> dp.Source.FromQuarantine
+            | ImportedCases -> fun dp -> dp.Source.Import
+            | ImportRelatedCases -> fun dp -> dp.Source.ImportRelated
+            | SourceUnknown -> fun dp -> dp.Source.Unknown
+            | LocalSource -> fun dp -> dp.Source.Local
 
-        let visible, color, seriesId = Series.getSeriesInfo (series)
-        {| ``type`` = "line"
-           visible = visible
+        let color, seriesId, stack = Series.getSeriesInfo (series)
+        {|
            color = color
            name = I18N.tt "charts.weeklyStats" seriesId
+           stack = stack
            data =
                state.data
                |> Seq.map (fun dp ->
                    {| x = dp.Date |> jsTime
                       y = getPoint dp
+                      fmtTotal = getPointTotal dp |> string
                       seriesId = seriesId
                       fmtWeekYearFromTo =
                           I18N.tOptions "days.weekYearFromToDate" {| date = dp.Date; dateTo = dp.DateTo |} |}
@@ -82,12 +137,27 @@ let renderChartOptions (state: State) dispatch =
 
         res
 
+    let className = "covid19-weekly-stats"
     let baseOptions =
-        basicChartOptions ScaleType.Linear "covid19-weeks" state.RangeSelectionButtonIndex onRangeSelectorButtonClick
+        basicChartOptions ScaleType.Linear className state.RangeSelectionButtonIndex onRangeSelectorButtonClick
+
+    let selectSeries displayType = match displayType with
+                                   | Quarantine -> Series.quarantine
+                                   | BySource -> Series.bySource
+                                   | BySourceCountry -> Series.bySource
 
     {| baseOptions with
+           chart = pojo
+            {|
+                animation = false
+                ``type`` = "column"
+                zoomType = "x"
+                className = className
+                events = pojo {| load = onLoadEvent(className) |}
+            |}
            series =
-               Series.all
+               state.displayType
+               |> selectSeries
                |> Seq.map (renderSeries)
                |> Seq.toArray
            yAxis =
@@ -108,7 +178,12 @@ let renderChartOptions (state: State) dispatch =
            legend =
                pojo
                    {| enabled = true
-                      layout = "horizontal" |} |}
+                      layout = "horizontal" |}
+           plotOptions = pojo {|
+                                column = pojo {|
+                                                stacking = "normal" |}
+
+                                |}|}
 
 let renderChartContainer state dispatch =
     Html.div [ prop.style [ style.height 450 ]
@@ -117,8 +192,26 @@ let renderChartContainer state dispatch =
                    [ renderChartOptions state dispatch
                      |> chartFromWindow ] ]
 
+let renderDisplaySelector state dt dispatch =
+    Html.div [
+        prop.onClick (fun _ -> ChangeDisplayType dt |> dispatch)
+        Utils.classes
+            [(true, "btn btn-sm metric-selector")
+             (state.displayType = dt, "metric-selector--selected") ]
+        prop.text (dt |> DisplayType.getName)
+    ]
+
+let renderDisplaySelectors state dispatch =
+    Html.div [
+        prop.className "metrics-selectors"
+        prop.children (
+            DisplayType.all
+            |> List.map (fun dt -> renderDisplaySelector state dt dispatch) ) ]
 let render (state: State) dispatch =
-    Html.div [ renderChartContainer state dispatch ]
+    Html.div [
+        renderChartContainer state dispatch
+        renderDisplaySelectors state dispatch
+    ]
 
 let weeklyStatsChart (props: {| data: WeeklyStatsData |}) =
     React.elmishComponent ("WeeklyStatsChart", init props.data, update, render)
