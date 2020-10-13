b4ae87493a6ecb85e43c929e3f7adcfcb68ba0ba
Andraz Vrhovec <andraz@koofr.net>
2020-10-13 21:51:22 +0000

    move markdown parsing from filter to directive

--- src/filters.js
+++ src/filters.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import marked from "marked"
 
-Vue.filter("prefixDiff", function(value) {
+Vue.filter("prefixDiff", function (value) {
   if (value > 0) {
     return `+${value}`;
   } else {
@@ -9,11 +9,15 @@ Vue.filter("prefixDiff", function(value) {
   }
 });
 
-// markdown filter
-// you can use it in Vue template and it will render markdown in user's browser
-// <div v-html="$options.filters.marked($t('charts.ostanizdrav.description'))"></div>
-// You have to use it with v-html directive, because moustache templating (stuf with `{{ ... }}`) will escape generated HTML
 
-Vue.filter("marked", function(input){
+/**
+ * markdown filter
+ * you can use it in Vue template and it will render markdown in user's browser
+ * <div v-html="$options.filters.marked($t('charts.ostanizdrav.description'))"></div>
+ * You have to use it with v-html directive, because moustache templating (stuf with `{{ ... }}`) will escape generated HTML
+ * 
+ * @deprecated use htmlMd directive (<div v-html-md="$t('charts.ostanizdrav.description')"></div>)
+ */
+Vue.filter("marked", function (input) {
   return marked(input)
 })
