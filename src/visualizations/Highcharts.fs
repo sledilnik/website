8b6888d5ccd93d93a97b2c8088c2c38879e9ee14
Luka Renko <46861689+lukarenko@users.noreply.github.com>
2020-10-17 08:34:06 +0200

    New phase: no contact tracing

--- src/visualizations/Highcharts.fs
+++ src/visualizations/Highcharts.fs
@@ -233,6 +233,7 @@ let basicChartOptions
                     {| value=jsTime <| DateTime(2020,5,15); label=Some {| text=I18N.t "phase.7.description"; rotation=270; align="right"; x=12 |} |}
                     {| value=jsTime <| DateTime(2020,9,10); label=Some {| text=I18N.t "phase.8.description"; rotation=270; align="right"; x=12 |} |}
                     {| value=jsTime <| DateTime(2020,10,9); label=Some {| text=I18N.t "phase.9.description"; rotation=270; align="right"; x=12 |} |}
+                    {| value=jsTime <| DateTime(2020,10,17);label=Some {| text=I18N.t "phase.10.description"; rotation=270; align="right"; x=12 |} |}
                 |]
                 plotBands=[|
                     {| ``from``=jsTime <| DateTime(2020,2,29);
@@ -276,9 +277,14 @@ let basicChartOptions
                        label=Some {| align="center"; text=I18N.t "phase.8.title" |}
                     |}
                     {| ``from``=jsTime <| DateTime(2020,10,9);
-                       ``to``=jsTime <| DateTime.Today;
+                       ``to``=jsTime <| DateTime(2020,10,17);
                        color="transparent"
                        label=Some {| align="center"; text=I18N.t "phase.9.title" |}
+                    |}
+                    {| ``from``=jsTime <| DateTime(2020,10,17);
+                       ``to``=jsTime <| DateTime.Today;
+                       color="transparent"
+                       label=Some {| align="center"; text=I18N.t "phase.10.title" |}
                     |}
                     yield! shadedWeekendPlotBands
                 |]
