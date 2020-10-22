22cd83034d1fb85ad9ae0aa0c9c20947860ef7df
Mia Erbus <mia.erbus@gmail.com>
2020-10-22 10:52:55 +0200

    #OstaniZdrav infocard remove excessive data

--- src/components/cards/OstaniZdravCard.vue
+++ src/components/cards/OstaniZdravCard.vue
@@ -3,36 +3,18 @@
     <div class="card-title d-flex justify-content-between">
       <span>{{ title }}</span>
     </div>
-    <div class="d-flex">
-      <div>
-        <div class="card-number">
-          <span>{{ lastValue.published }}</span>
-          <div class="card-percentage-diff" :class="diffClass">
-            {{ diff | prefixDiff }}%
-          </div>
-        </div>
-        <div class="card-diff">
-          <div><span class="card-note">published</span></div>
-        </div>
-      </div>
-      <div class="ml-0 ml-sm-5">
-        <div class="card-number">
-          <span>{{ lastValue.users_published }}</span>
-          <div class="card-percentage-diff" :class="diffClassUsers">
-            {{ diffUsers | prefixDiff }}%
-          </div>
-        </div>
-        <div class="card-diff">
-          <div><span class="card-note">users</span></div>
-        </div>
+    <div class="card-number">
+      <span>{{ lastValue }}</span>
+      <div class="card-percentage-diff" :class="diffClass">
+        {{ percentageDiff | prefixDiff }}%
       </div>
     </div>
+    <div class="card-diff">
+      <div class="trend-icon" :class="[diffClass, iconClass]"></div>
+      <span :class="diffClass">{{ diff }}</span>
+    </div>
     <div class="data-time">
-      {{
-        $t('infocard.lastUpdated', {
-          date: new Date(lastValue.date),
-        })
-      }}
+      {{ $t('infocard.lastUpdated', { date: new Date(date) }) }}
     </div>
   </div>
 </template>
@@ -47,32 +29,48 @@ export default {
   computed: {
     ...mapGetters('ostaniZdrav', ['data']),
     ...mapState('ostaniZdrav', ['loaded']),
+    date() {
+      return this.data[this.data.length - 2].date
+    },
     lastValue() {
-      const lastValue = this.data[this.data.length - 1]
-      return lastValue
+      const lastValue = this.data[this.data.length - 2]
+      return lastValue.users_published
+    },
+    dayBeforeValue() {
+      const dayBeforeValue = this.data[this.data.length - 3]
+      return dayBeforeValue.users_published
     },
     diff() {
-      const lastValue = this.data[this.data.length - 1]
-      const oneBeforeLastValue = this.data[this.data.length - 2]
-      return lastValue.published - oneBeforeLastValue.published
+      const lastValue = this.data[this.data.length - 2].users_published
+      const dayBeforeLastValue = this.data[this.data.length - 3].users_published
+      return lastValue - dayBeforeLastValue
     },
-    diffUsers() {
-      const lastValue = this.data[this.data.length - 1]
-      const oneBeforeLastValue = this.data[this.data.length - 2]
-      return lastValue.users_published - oneBeforeLastValue.users_published
+    dayBeforeDiff() {
+      const lastValue = this.data[this.data.length - 3].users_published
+      const dayBeforeLastValue = this.data[this.data.length - 4].users_published
+      return lastValue - dayBeforeLastValue
+    },
+    percentageDiff() {
+      console.log(this.diff, this.dayBeforeValue)
+      return Math.round((this.diff / this.dayBeforeValue) * 1000) / 10
+    },
+    iconClass() {
+      let className = ''
+      if (this.diff === 0) {
+        className += ' none'
+        return className
+      } else if (this.diff > 0) {
+        className += ' up'
+      } else {
+        className += ' down'
+      }
+      return className
     },
     diffClass() {
       if (this.diff === 0) {
         return 'no-change'
       } else {
         return this.diff > 0 ? 'good' : 'bad'
-      }
-    },
-    diffClassUsers() {
-      if (this.diffUsers === 0) {
-        return 'no-change'
-      } else {
-        return this.diffUsers > 0 ? 'good' : 'bad'
       }
     },
   },
