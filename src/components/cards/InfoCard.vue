8c08d85eff3c17bdc5da5d281be2effb20ed1dc8
Mia Erbus <mia.erbus@gmail.com>
2020-11-01 13:59:30 +0100

    obsolete translations

--- src/components/cards/InfoCard.vue
+++ src/components/cards/InfoCard.vue
@@ -123,25 +123,6 @@ export default {
         return this.goodTrend === 'down' ? 'good' : 'bad'
       }
     },
-    incidenceClass() {
-      const value = this.renderValues.lastDay.value
-      const incidence = Math.round(
-        this.renderValues.lastDay.value / this.incidence
-      )
-      if (this.name === 'incidence') {
-        if (incidence >= 40 && incidence < 140) return 'orange'
-        if (incidence >= 140) return 'red'
-      }
-      if (this.field === 'statePerTreatment.inHospital') {
-        if (value >= 60 && value < 250) return 'orange'
-        if (value >= 250) return 'red'
-      }
-      if (this.field === 'statePerTreatment.inICU') {
-        if (value >= 15 && value < 50) return 'orange'
-        if (value >= 50) return 'red'
-      }
-      return 'unknown'
-    },
     cardTitle() {
       if (this.name === 'incidence')
         return this.title + ' ' + this.$t('infocard.per100k')
