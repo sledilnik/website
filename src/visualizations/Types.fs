7828eeb3266eafbfd3288fa728383766aa1d2578
Luka Renko <46861689+lukarenko@users.noreply.github.com>
2020-11-04 08:15:49 +0100

    Weekly: added investigated and healthcare cases

--- src/visualizations/Types.fs
+++ src/visualizations/Types.fs
@@ -94,6 +94,8 @@ type WeeklyStatsDataPoint =
       Date : System.DateTime
       DateTo : System.DateTime
       ConfirmedCases : int option
+      InvestigatedCases : int option
+      HealthcareCases : int option
       SentToQuarantine : int option
       Source : InfectionSource
       ImportedFrom : Map<string, int option>
