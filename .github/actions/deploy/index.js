aa9139fe5290da8605034caae96be561ceeccbcf
Andraz Vrhovec <andraz@koofr.net>
2020-11-26 15:47:52 +0000

    wip

--- .github/actions/deploy/index.js
+++ .github/actions/deploy/index.js
@@ -1,7 +1,7 @@
 const github = require('@actions/github')
 const core = require('@actions/core')
 
-const ghToken = process.env['ACTIONS_RUNTIME_TOKEN']
+const ghToken = process.env['INPUT_TOKEN']
 
 console.log("ghToken", ghToken)
 
