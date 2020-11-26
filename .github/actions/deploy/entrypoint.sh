bf26d24655f71d662c1508f676ab2b44acfe5fe1
Andraz Vrhovec <andraz@koofr.net>
2020-11-26 02:16:02 +0000

    wip

--- .github/actions/deploy/entrypoint.sh
+++ .github/actions/deploy/entrypoint.sh
@@ -6,6 +6,6 @@ echo "${INPUT_KUBECONFIG}" > ~/.kube/config
 helm upgrade ${INPUT_RELEASE_NAME} ./helm-chart \
     --install \
     --atomic \
-    --namespace=${ INPUT_NAMESPACE } \
-    --set=image.tag= \
+    --namespace=${INPUT_NAMESPACE} \
+    --set=image.tag=${IMAGE_TAG} \
     --set=ingress.rule="${INPUT_INGRESS_RULE}"
