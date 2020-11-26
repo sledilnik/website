d1d6c02c82acf2efa6520478aa0b0fb094523db1
Andraz Vrhovec <andraz@koofr.net>
2020-11-26 09:12:45 +0000

    wip

--- .github/actions/deploy/entrypoint.sh
+++ .github/actions/deploy/entrypoint.sh
@@ -3,9 +3,25 @@
 mkdir -p ${HOME}/.kube/
 echo "${INPUT_KUBECONFIG}" > ${HOME}/.kube/config
 
-helm list --namespace ${INPUT_NAMESPACE}
 
-echo `which curl`
+create_deployment() {
+    curl --request POST \
+        --url https://api.github.com/repos/overlordtm/data-api/deployments \
+        --header "Authorization: Bearer ${INPUT_TOKEN}" \
+        --header 'Content-Type: application/json' \
+        --data '{
+            "ref": "${GITHUB_REF}",
+            "environment": "pr-1",
+            "payload": {
+                "releaseName": "data-api-pr-1",
+                "namespace": "default"
+            }
+        }'
+}
+
+create_deployment 
+
+helm list --namespace ${INPUT_NAMESPACE}
 
 # helm upgrade ${INPUT_RELEASE_NAME} ./helm-chart \
 #     --install \
