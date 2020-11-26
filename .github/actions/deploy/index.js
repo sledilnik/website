2a3082146368fef41734c5db8d6a0474d3225174
Andraz Vrhovec <andraz@koofr.net>
2020-11-26 17:43:43 +0000

    wip

--- .github/actions/deploy/index.js
+++ .github/actions/deploy/index.js
@@ -19,7 +19,7 @@ async function createDeployment() {
 }
 
 async function helmDeploy() {
-    let x = execFileSync("helm", ["--list"])
+    let x = execFileSync("/usr/bin/helm", ["--list"])
     core.info("neki", x)
 }
 
