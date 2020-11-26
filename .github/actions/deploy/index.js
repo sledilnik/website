60f95044f145e66d50d722c8ed346a3a5fedd575
Andraz Vrhovec <andraz@koofr.net>
2020-11-26 16:30:50 +0000

    wip

--- .github/actions/deploy/index.js
+++ .github/actions/deploy/index.js
@@ -1,19 +1,25 @@
 const github = require('@actions/github')
 const core = require('@actions/core')
+const { execFileSync } = require('child_process')
 
 const ghToken = process.env['INPUT_TOKEN']
 
 const context = github.context;
+// core.info(JSON.stringify(context.payload, null, 1))
 const gh = github.getOctokit(ghToken)
 
-function createDeployment() {
-    core.info(JSON.stringify(context.payload, null, 1))
-    gh.repos.createDeployment({
+async function createDeployment() {
+    return await gh.repos.createDeployment({
         owner: context.payload.repository.owner.login,
         repo: context.payload.repository.name,
         ref: context.ref,
         environment: "testenv",
     })
+}
+
+async function neki() {
+    let x = await execFileSync("helm", ["--list"])
+    core.info(x)
 }
 
 function main() {
