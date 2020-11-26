1d73e22cdb2616c254f7a3d59982e1703798822a
Andraz Vrhovec <andraz@koofr.net>
2020-11-26 02:01:39 +0000

    wi

--- .github/actions/deploy/entrypoint.sh
+++ .github/actions/deploy/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+env
+
+echo "${INPUT_KUBECONFIG}" > ~/.kube/config
+
+helm upgrade ${INPUT_RELEASE_NAME} ./helm-chart \
+    --install \
+    --atomic \
+    --namespace=${{ INPUT_NAMESPACE }} \
+    --set=image.tag= \
+    --set=ingress.rule='Host(`${{ env.DEPLOYMENT_URL }}`)' \
