878cb97a2381a1e004dcb0336677236b91f01b12
Mia Erbus <mia.erbus@gmail.com>
2020-12-07 19:47:21 +0100

    style single post

--- src/pages/PostSingle.vue
+++ src/pages/PostSingle.vue
@@ -1,20 +1,32 @@
 <template>
-  <div class="container">
-    <template v-if="post">
-      <h1>{{ post.title }}</h1>
-      <div v-html="post.body"></div>
-    </template>
-    <Loader v-else />
+  <div class="custom-container">
+    <div class="static-page-wrapper">
+      <template v-if="post">
+        <h1>{{ post.title }}</h1>
+        <div class="info-text">
+          <span v-if="post.author" class="author">{{ post.author }} · </span>
+          <span class="date-time">{{ $t("posts.timestamp", { date }) }}</span>
+          <reading-time :content="post.body"></reading-time>
+        </div>
+        <div class="content" v-html="post.body"></div>
+        <div class="btn-wrapper">
+          <a class="btn" @click="goBack">{{ $t("pageNotFound.back") }}</a>
+        </div>
+      </template>
+      <Loader v-else />
+    </div>
   </div>
 </template>
 
 <script>
 import _ from "lodash";
 import Loader from "components/Loader";
+import ReadingTime from "components/ReadingTime";
 
 export default {
   components: {
     Loader,
+    ReadingTime
   },
   data() {
     return {
@@ -24,6 +36,11 @@ export default {
   metaInfo() {
     //TODO
   },
+  computed: {
+    date() {
+      return new Date(this.post.created);
+    },
+  },
   async created() {
     const postId = _.get(this.$route, "params.postId", false);
     if (!postId) {
@@ -31,7 +48,43 @@ export default {
     }
     this.post = await this.contentApi.get(`/posts/${postId}`);
   },
+  methods: {
+    goBack() {
+      return this.$router.go(-1);
+    }
+  }
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.info-text {
+  font-size: 14px;
+  color: rgba(117, 117, 117, 1);
+}
+
+.content {
+  margin-top: 30px;
+}
+
+.btn-wrapper {
+  margin-top: 30px;
+}
+
+.btn {
+  display: inline-block;
+  color: #000;
+  font-size: 14px;
+  line-height: 16px;
+  font-weight: bold;
+  border-radius: 6px;
+  padding: 11px 12px;
+  border: solid 1px rgba(0, 0, 0, 0.13);
+  cursor: pointer;
+  background: $yellow;
+  border: none;
+
+  &:hover {
+    text-decoration: none;
+  }
+}
+</style>
