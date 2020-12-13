2f2737d5ddc136c5918da5a919a36f19dfb82f25
Demjan <demjan.vester@gmail.com>
2020-12-13 01:16:34 +0100

    .

--- src/services/content-api.service.js
+++ src/services/content-api.service.js
@@ -2,16 +2,9 @@ import ApiService from "./api.service";
 import { setupCache } from "axios-cache-adapter";
 
 export const CONTENT_ENDPOINT_BASE = process.env.VUE_APP_CONTENT_ENDPOINT_BASE;
-const FIFTEEN_MINUTES = 15 * 60 * 1000;
-const defaultCache = {
-  maxAge: FIFTEEN_MINUTES,
-  exclude: {
-    query: false,
-  },
-};
 
 class ContentApiService extends ApiService {
-  constructor({ cache = defaultCache } = {}) {
+  constructor({ cache } = {}) {
     const cacheObj = cache ? setupCache(cache) : undefined;
     super({ baseURL: CONTENT_ENDPOINT_BASE, cache: cacheObj });
   }
