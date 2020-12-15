fa68f5eeeceda2c228ea1c270c8a07a2c592e36f
Mia Erbus <mia.erbus@gmail.com>
2020-12-15 15:21:14 +0100

    fix new cases diff percentage color

--- src/components/cards/InfoCard.vue
+++ src/components/cards/InfoCard.vue
@@ -5,7 +5,7 @@
       <div class="card-number">
         <span v-if="showRunningSum">{{ renderRunningSum }}</span>
         <span v-else>{{ renderValues.lastDay.value }}</span>
-        <div v-if="showRunningSum" class="card-percentage-diff" :class="diffClass">
+        <div v-if="showRunningSum" class="card-percentage-diff" :class="diffSumClass">
           {{ renderRunningSumDiff | prefixDiff }}%
         </div>
         <div v-else class="card-percentage-diff" :class="diffClass">
@@ -114,6 +114,15 @@ export default {
         return this.goodTrend === 'down' ? 'bad' : 'good'
       } else {
         return this.goodTrend === 'down' ? 'good' : 'bad'
+      }
+    },
+    diffSumClass() {
+      if (this.renderRunningSumDiff === 0) {
+        return 'no-change'
+      } else if (this.renderRunningSumDiff > 0) {
+        return 'bad'
+      } else {
+        return 'good'
       }
     },
     cardTitle() {
