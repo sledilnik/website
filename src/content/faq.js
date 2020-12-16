9b78d5fe295131e3f03ca91d5ad4d7da9779bcd6
Andraz Vrhovec <andraz@vrhovec.si>
2020-12-16 14:55:41 +0100

    Search POC (#756)
    * Search POC
    
    * fix v-html-md directive not to crash if null is provided
    
    * searchable faq
    
    * more localization
    
    * fixup! more localization
    
    * some style
    
    * Use new action for pr deploys
    
    * No more comments on PR
    
    Co-authored-by: Andraz Vrhovec <andraz@koofr.net>

---
+++ src/content/faq.js
@@ -0,0 +1,79 @@
+export default [
+    {
+        id: "general",
+        ids: [
+            "why-sledilnik",
+            "virus-vs-disease",
+            "ventilator-vs-respirator",
+            "confirmed-cases",
+            "all-infected",
+            "other-countries",
+            "english-translation",
+            "are-you-paid",
+            "tech-used"
+        ]
+    },
+    {
+        id: "data",
+        ids: [
+            "restrictions",
+            "data-reliability",
+            "data-collection",
+            "data-publish-time",
+            "data-differences",
+            "data-hospital-in",
+            "data-hospital-out",
+            "data-recovered",
+            "data-active-cases",
+            "data-contribute"
+        ],
+    },
+    {
+        id: "charts",
+        ids: [
+            "data-usage",
+            "chart-usage",
+            "chart-infocard-percent",
+            "chart-infocard-phase",
+            "metrics-comparison-chart",
+            "chart-metrics-included",
+            "chart-terminology",
+            "cases-chart",
+            "chart-phases",
+            "patients-chart",
+            "care-patients-chart",
+            "deceased-chart",
+            "excess-deaths-chart",
+            "daily-comparison-chart",
+            "sources-chart",
+            "ratios-chart",
+            "hcenters-chart",
+            "tests-chart",
+            "infections-chart",
+            "spread-chart",
+            "regions-chart-100k",
+            "regions-chart",
+            "map-chart",
+            "rmap-chart",
+            "chart-double-rate",
+            "municipalities-chart",
+            "phase-diagram-chart",
+            "europe-chart",
+            "age-groups-chart",
+            "world-chart",
+            "countries-cases-chart",
+            "countries-deaths-chart",
+            "chart-reality",
+            "chart-percentage",
+            "chart-log-scale"
+        ]
+    },
+    {
+        id: "project",
+        ids: [
+            "chart-exp-growth",
+            "what-is-sledilnik",
+            "how-to-help"
+        ]
+    }
+]
