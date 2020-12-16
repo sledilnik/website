cef4aed4c311bdc967cb54dd204ba5d8a45b3eb4
Joh Dokler <joh.dokler@gmail.com>
2020-12-16 15:22:16 +0100

    Add bindings for Intl.NumberFormat

--- src/visualizations/I18N.fs
+++ src/visualizations/I18N.fs
@@ -1,5 +1,6 @@
 module I18N
 
+open Fable.Core
 open Fable.Core.JsInterop
 
 type Ii18n =
@@ -20,3 +21,28 @@ let dow dayOfWeek =
 
 let chartText chartTextsGroup textId =
     t ("charts." + chartTextsGroup + "." + textId)
+
+
+open Browser
+
+[<Erase>]
+type Number =
+    | Int of int
+    | Float of float
+
+[<ImportMember("i18n")>]
+let formatNumber (x : Number) : string = jsNative
+
+// printfn "%s" (formatNumber (Int 123456))
+// printfn "%s" (formatNumber (Float 123456.789))
+
+
+type NumberFormat =
+    abstract formatNumber : int -> string
+    abstract formatNumber : float -> string
+
+[<ImportAll("i18n")>]
+let NumberFormat : NumberFormat = jsNative
+
+// printfn "%s" (NumberFormat.formatNumber 123456)
+// printfn "%s" (NumberFormat.formatNumber 123456.789)
