3a7f34943c9e32a74aab4be19e97dce24a3e82a5
Andraz Vrhovec <andraz@koofr.net>
2020-12-29 17:43:05 +0100

    Prerender less routes

--- build.prerender.js
+++ build.prerender.js
@@ -9,19 +9,20 @@ const cartesian =
 async function generatePrerenderRoutes() {
     const langs = ['sl', 'en']
     const paths = ['stats', 'world', 'restrictions', 'about', 'faq', 'posts', 'data', 'ostanizdrav']
-    const basicRoutes = cartesian(langs, paths).map(pair => `/${pair.join('/')}`).map(s => s.replace(/\/$/, ''))
+    const basicRoutes = cartesian(langs, paths).map(pair => `/${pair.join('/')}`)
 
     const { data } = await axios.get('https://backend.sledilnik.org/api/v1/posts')
-    const postRoutes = cartesian(langs, data.objects.map(post => `posts/${post.id}`)).map(pair => `/${pair.join('/')}`).map(s => s.replace(/\/$/, ''))
+    const postRoutes = cartesian(langs, data.objects.map(post => `posts/${post.id}`)).map(pair => `/${pair.join('/')}`)
 
-    return basicRoutes.concat(postRoutes)
+    // return basicRoutes.concat(postRoutes)
+    return basicRoutes
 }
 
 module.exports = (api, options) => {
     api.registerCommand('build:prerender', async (args) => {
         //   const PrerenderSPAPlugin = require('prerender-spa-plugin')
         const prerenderRoutes = await generatePrerenderRoutes()
-        console.log(`Prerendering URLs ${prerenderRoutes}`)
+        console.log(`Prerendering ${prerenderRoutes.length} URLs ${prerenderRoutes}`)
 
         api.chainWebpack(config => {
             config.plugin('prerender').use(PrerenderSPAPlugin, [{
