4d4099d051e45a4e3f30da10172a81b48b1255a3
Joh Dokler <joh.dokler@gmail.com>
2020-12-29 19:24:34 +0100

    Add multiple years support for ExcessDeaths chart

--- src/visualizations/ExcessDeathsChart/Absolute.fs
+++ src/visualizations/ExcessDeathsChart/Absolute.fs
@@ -7,13 +7,13 @@ open Types
 open Browser
 
 let colors = {|
+    Year = "#a0a0a0"
     CurrentYear = "#a483c7"
     BaselineYear = "#d5d5d5"
 |}
 
 let renderChartOptions (data : WeeklyDeathsData) =
     let minYear = 2010 // used to filter out 2009 data tail
-
     let series =
         data
         |> List.groupBy (fun dp -> dp.Year)
@@ -30,10 +30,16 @@ let renderChartOptions (data : WeeklyDeathsData) =
             {| ``type`` = "line"
                name = year
                animation = false
-               showInLegend = year = System.DateTime.Now.Year
+               showInLegend = year >= START_YEAR
                data = seriesData
-               color = if year = System.DateTime.Now.Year then colors.CurrentYear else colors.BaselineYear
-               lineWidth = if year = System.DateTime.Now.Year then 2 else 1
+               color =
+                if year = System.DateTime.Now.Year then
+                    colors.CurrentYear
+                elif year >= START_YEAR then
+                    colors.Year
+                else
+                    colors.BaselineYear
+               lineWidth = if year >= START_YEAR then 2 else 1
                marker = {| enabled = true ; symbol = "circle" ; radius = 2 |} |> pojo
             |} |> pojo)
         |> List.toArray
