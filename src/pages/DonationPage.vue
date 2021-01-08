0d27d70924fce3c4bd335637061b063209edaa14
Stefan Baebler <stefan.baebler@gmail.com>
2021-01-08 12:54:07 +0100

    Added bank transfer

--- src/pages/DonationPage.vue
+++ src/pages/DonationPage.vue
@@ -2,6 +2,10 @@
   <div class="custom-container">
     <div class="static-page-wrapper">
       <h1>{{ $t("donation.title") }}</h1>
+      <div class="intro" v-html-md="$t('donation.intro')" />
+
+      <h2>{{ $t("donation.monthly.title") }}</h2>
+      <div class="intro" v-html-md="$t('donation.monthly.description')" />
       <div>
         <stripe-checkout
           ref="checkoutRef"
@@ -14,12 +18,48 @@
         />
         <button @click="submit">{{ $t("donation.buttonSubscribe") }}</button>
       </div>
+
+      <h2>{{ $t("donation.banktransfer.title") }}</h2>
+      <div class="intro" v-html-md="$t('donation.banktransfer.description')" />
+      <table>
+        <tr>
+          <td>{{ $t("donation.banktransfer.recipient") }}</td>
+          <td>Znanstveno društvo Sledilnik<br>Celovška cesta 111<br>Ljubljana<br>Slovenija</td>
+        </tr>
+        <tr>
+          <td>{{ $t("donation.banktransfer.iban") }}</td>
+          <td>SI56 6100 0002 5152 059</td>
+        </tr>
+        <tr>
+          <td>{{ $t("donation.banktransfer.purpose") }}</td>
+          <td>CHAR</td>
+        </tr>
+        <tr>
+          <td>{{ $t("donation.banktransfer.bankBicSwift") }}</td>
+          <td>HDELSI22</td>
+        </tr>
+        <tr>
+          <td>{{ $t("donation.banktransfer.bank") }}</td>
+          <td>Delavska hranilnica d.d.<br>Miklošičeva 5<br>Ljubljana<br>Slovenia</td>
+        </tr>
+
+      </table>
+
+      <img src="../assets/donate-qr.png" class="qr" />
+      <img src="../assets/donate-upn.png" class="upn" />
+
+      <div class="intro" v-html-md="$t('donation.outro')" />
     </div>
   </div>
 </template>
 
 <script>
 import { StripeCheckout } from '@vue-stripe/vue-stripe';
+import AgeGroupsFemales from '../components/tables/AgeGroupsFemales.vue';
+
+console.log(process.env.VUE_APP_CONTENT_ENDPOINT_BASE)
+console.log(process.env.VUE_APP_DONATEPK)
+console.log(process.env.VUE_APP_LOCALE_CONTEXT)
 
 export default {
   name: 'DonationPage',
@@ -50,4 +90,10 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss"></style>
+<style lang="scss">
+
+.upn {
+  max-width: 100%;
+}
+
+</style>
