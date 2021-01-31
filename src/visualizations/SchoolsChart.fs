47d660f36dc4bb5ec172c20a4b247e7dada541c1
Luka Renko <46861689+lukarenko@users.noreply.github.com>
2021-01-31 10:04:25 +0100

    Add stacked bar chart by type

--- src/visualizations/SchoolsChart.fs
+++ src/visualizations/SchoolsChart.fs
@@ -26,24 +26,41 @@ with
         | Active -> chartText "active"
         | Today -> chartText "today"
 
-
-let DefaultDisplayType = "kindergarten" 
-let AllDisplayTypes = [ 
-    "kindergarten"
-    "elementary"
-    "highschool"
-    "elementary_special"
-    "music"
-    "dormitory"
-    "institutions"
+let AllSchoolTypes = [ 
+    "kindergarten",         "#ffa600" 
+    "elementary",           "#70a471"
+    "highschool",           "#a05195"
+    "elementary_special",   "#457844"
+    "music",                "#f95d6a"
+    "dormitory",            "#665191"
+    "institutions",         "#10829a"
 ]
+
+type DisplayType =
+    | Attendees
+    | Employees
+    | Type of string
+    static member All =
+        seq {
+            yield Attendees
+            yield Employees
+            for typ, color in AllSchoolTypes do
+                yield Type typ
+        }
+    static member Default = Attendees
+    static member GetName =
+        function
+        | Attendees -> chartText "attendeeCases"
+        | Employees -> chartText "employeeCases"
+        | Type typ  -> chartText typ
+
 
 type State =
     { SchoolsData: SchoolsStats array
       Error: string option
       ScaleType: ScaleType
       MetricType: MetricType
-      DisplayType: string
+      DisplayType: DisplayType
       RangeSelectionButtonIndex: int }
 
 
@@ -52,7 +69,7 @@ type Msg =
     | ConsumeServerError of exn
     | ScaleTypeChanged of ScaleType
     | MetricTypeChanged of MetricType
-    | ChangeDisplayType of string
+    | ChangeDisplayType of DisplayType
     | RangeSelectionChanged of int
 
 let init: State * Cmd<Msg> =
@@ -64,7 +81,7 @@ let init: State * Cmd<Msg> =
           Error = None
           ScaleType = Linear
           MetricType = MetricType.Default
-          DisplayType = DefaultDisplayType
+          DisplayType = DisplayType.Default
           RangeSelectionButtonIndex = 0 }
 
     state, cmd
@@ -86,76 +103,101 @@ let update (msg: Msg) (state: State): State * Cmd<Msg> =
     | RangeSelectionChanged buttonIndex ->
         { state with RangeSelectionButtonIndex = buttonIndex }, Cmd.none
 
-let renderChartOptions state dispatch =
+
+let chartCredits =
+    {| 
+        enabled = true
+        text = sprintf "%s: %s, %s"
+                    (I18N.t "charts.common.dataSource")
+                    (I18N.tOptions ("charts.common.dsMIZS") {| context = localStorage.getItem ("contextCountry") |})
+                    (I18N.tOptions ("charts.common.dsNIJZ") {| context = localStorage.getItem ("contextCountry") |})
+        href = "https://www.gov.si/drzavni-organi/ministrstva/ministrstvo-za-izobrazevanje-znanost-in-sport/" 
+    |}
+
+let renderPerSchoolChart state dispatch =
 
     let allSeries =
+        let sType =
+            match state.DisplayType with
+            | Type typ -> typ
+            | _ -> ""
+
         match state.MetricType with
         | Active ->
             [ yield
                 pojo
                     {| name = chartText "employeesCases"
+                       ``type`` = "line"
                        color = "#dba51d"
                        data =
                            state.SchoolsData
-                           |> Array.map (fun dp -> (dp.JsDate12h, dp.schoolType.[state.DisplayType].employees.active |> Utils.zeroToNone)) |}
+                           |> Array.map (fun dp -> (dp.JsDate12h, dp.schoolType.[sType].employees.active |> Utils.zeroToNone)) |}
               yield
                 pojo
                     {| name = chartText "attendeesCases"
+                       ``type`` = "line"
                        color = "#bda506"
                        data =
                            state.SchoolsData
-                           |> Array.map (fun dp -> (dp.JsDate12h, dp.schoolType.[state.DisplayType].attendees.active |> Utils.zeroToNone)) |}
+                           |> Array.map (fun dp -> (dp.JsDate12h, dp.schoolType.[sType].attendees.active |> Utils.zeroToNone)) |}
               yield
                 pojo
                     {| name = chartText "attendeesQuarantine"
+                       ``type`` = "line"
                        color = "#cccccc"
                        data =
                            state.SchoolsData
-                           |> Array.map (fun dp -> (dp.JsDate12h, dp.schoolType.[state.DisplayType].attendees.quarantined |> Utils.zeroToNone)) |}
+                           |> Array.map (fun dp -> (dp.JsDate12h, dp.schoolType.[sType].attendees.quarantined |> Utils.zeroToNone)) |}
               yield
                 pojo
                     {| name = chartText "unitsQuarantine"
+                       ``type`` = "line"
                        color = "#f4b2e0"
                        data =
                            state.SchoolsData
-                           |> Array.map (fun dp -> (dp.JsDate12h, dp.schoolType.[state.DisplayType].units.quarantined |> Utils.zeroToNone)) |}
+                           |> Array.map (fun dp -> (dp.JsDate12h, dp.schoolType.[sType].units.quarantined |> Utils.zeroToNone)) |}
               yield
                 pojo
                     {| name = chartText "unitsRemote"
+                       ``type`` = "line"
                        color = "#d559b0"
                        data =
                            state.SchoolsData
-                           |> Array.map (fun dp -> (dp.JsDate12h, dp.schoolType.[state.DisplayType].units.remote |> Utils.zeroToNone))  |}
+                           |> Array.map (fun dp -> (dp.JsDate12h, dp.schoolType.[sType].units.remote |> Utils.zeroToNone))  |}
             ]
         | Today ->
             [ yield
                 pojo
                     {| name = chartText "employeesCases"
+                       ``type`` = "line"
                        color = "#dba51d"
                        data =
                            state.SchoolsData
-                           |> Array.map (fun dp -> (dp.JsDate12h, dp.schoolType.[state.DisplayType].employees.confirmed)) |}
+                           |> Array.map (fun dp -> (dp.JsDate12h, dp.schoolType.[sType].employees.confirmed)) |}
               yield
                 pojo
                     {| name = chartText "attendeesCases"
+                       ``type`` = "line"
                        color = "#bda506"
                        data =
                            state.SchoolsData
-                           |> Array.map (fun dp -> (dp.JsDate12h, dp.schoolType.[state.DisplayType].attendees.confirmed)) |}
+                           |> Array.map (fun dp -> (dp.JsDate12h, dp.schoolType.[sType].attendees.confirmed)) |}
               yield
                 pojo
                     {| name = chartText "attendeesQuarantine"
+                       ``type`` = "line"
                        color = "#cccccc"
                        data =
                            state.SchoolsData
-                           |> Array.map (fun dp -> (dp.JsDate12h, dp.schoolType.[state.DisplayType].attendees.to_quarantine)) |}
+                           |> Array.map (fun dp -> (dp.JsDate12h, dp.schoolType.[sType].attendees.to_quarantine)) |}
               yield
                 pojo
                     {| name = chartText "unitsQuarantine"
+                       ``type`` = "line"
                        color = "#f4b2e0"
                        data =
                            state.SchoolsData
-                           |> Array.map (fun dp -> (dp.JsDate12h, dp.schoolType.[state.DisplayType].units.to_quarantine)) |}
+                           |> Array.map (fun dp -> (dp.JsDate12h, dp.schoolType.[sType].units.to_quarantine)) |}
             ]
 
     let onRangeSelectorButtonClick(buttonIndex: int) =
@@ -174,20 +216,72 @@ let renderChartOptions state dispatch =
             let showFirstLabel = state.ScaleType <> Linear
             baseOptions.yAxis |> Array.map (fun ax -> {| ax with showFirstLabel = Some showFirstLabel |})
         legend = pojo {| enabled = true ; layout = "horizontal" |}
-        credits =
-            {| enabled = true
-               text = sprintf "%s: %s, %s"
-                    (I18N.t "charts.common.dataSource")
-                    (I18N.tOptions ("charts.common.dsMIZS") {| context = localStorage.getItem ("contextCountry") |})
-                    (I18N.tOptions ("charts.common.dsNIJZ") {| context = localStorage.getItem ("contextCountry") |})
-               href = "https://www.gov.si/drzavni-organi/ministrstva/ministrstvo-za-izobrazevanje-znanost-in-sport/" |} |> pojo
+        credits = pojo chartCredits
+    |}
+
+
+let renderStackedChart state dispatch =
+
+    let allSeries = seq {
+        for sType, sColor in AllSchoolTypes do
+            yield
+                pojo
+                    {| name = chartText sType
+                       ``type`` = "column"
+                       color = sColor
+                       data =
+                           state.SchoolsData
+                           |> Array.map (fun dp -> (dp.JsDate12h, 
+                                                    match state.DisplayType, state.MetricType with
+                                                    | Attendees, Active -> 
+                                                        dp.schoolType.[sType].attendees.active |> Utils.zeroToNone
+                                                    | Attendees, Today -> 
+                                                        dp.schoolType.[sType].attendees.confirmed |> Utils.zeroToNone
+                                                    | Employees, Active -> 
+                                                        dp.schoolType.[sType].employees.active |> Utils.zeroToNone 
+                                                    | Employees, Today -> 
+                                                        dp.schoolType.[sType].employees.confirmed |> Utils.zeroToNone 
+                                                    | _, _ -> None)) |} 
+    }
+
+    let onRangeSelectorButtonClick(buttonIndex: int) =
+        let res (_ : Event) =
+            RangeSelectionChanged buttonIndex |> dispatch
+            true
+        res
+
+    let baseOptions =
+        basicChartOptions state.ScaleType "covid19-schools"
+            state.RangeSelectionButtonIndex
+            onRangeSelectorButtonClick
+    {| baseOptions with
+        series = Seq.toArray allSeries
+        yAxis =
+            let showFirstLabel = state.ScaleType <> Linear
+            baseOptions.yAxis |> Array.map (fun ax -> {| ax with showFirstLabel = Some showFirstLabel |})
+        plotOptions =
+            pojo
+               {| column = pojo {| dataGrouping = pojo {| enabled = false |} |}
+                  series =
+                      {| stacking = "normal"
+                         crisp = false
+                         borderWidth = 0
+                         pointPadding = 0
+                         groupPadding = 0 |} |}
+        legend = pojo {| enabled = true ; layout = "horizontal" |}
+        credits = pojo chartCredits
     |}
 
 let renderChartContainer (state: State) dispatch =
     Html.div [ prop.style [ style.height 480 ]
                prop.className "highcharts-wrapper"
-               prop.children [ renderChartOptions state dispatch |> Highcharts.chart ] ]
-
+               prop.children [ 
+                    match state.DisplayType with
+                    | Type typ ->
+                        renderPerSchoolChart state dispatch |> Highcharts.chartFromWindow
+                    | _ ->
+                        renderStackedChart state dispatch |> Highcharts.chartFromWindow
+               ] ]
 
 let renderMetricTypeSelectors (activeMetricType: MetricType) dispatch =
     let renderMetricTypeSelector (metricTypeToRender: MetricType) =
@@ -211,17 +305,17 @@ let renderMetricTypeSelectors (activeMetricType: MetricType) dispatch =
 
 
 let renderDisplaySelectors state dispatch =
-    let renderSelector state (dt: string) dispatch =
+    let renderSelector (dt: DisplayType) dispatch =
         Html.div [ let isActive = state.DisplayType = dt
                    prop.onClick (fun _ -> ChangeDisplayType dt |> dispatch)
                    Utils.classes [ (true, "btn btn-sm metric-selector")
                                    (isActive, "metric-selector--selected") ]
-                   prop.text (chartText dt) ]
+                   prop.text (DisplayType.GetName dt) ]
 
     Html.div [ prop.className "metrics-selectors"
                prop.children
-                   (AllDisplayTypes
-                    |> Seq.map (fun dt -> renderSelector state dt dispatch)) ]
+                   (DisplayType.All
+                    |> Seq.map (fun dt -> renderSelector dt dispatch)) ]
 
 
 let render (state: State) dispatch =
