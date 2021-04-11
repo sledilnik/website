3bb8d6de178b22de3bbaba9885c45ea848166bbd
Štefan Baebler <stefan.baebler@gmail.com>
2021-04-11 11:05:53 +0200

    Re-enable vaccinations chart (#872)
    * Re-enable vaccinations chart
    
    * Revert "Hide vaccination in default view"
    
    This reverts commit 90ca76ce3c0fb9e55b22e07200b472ba3892e32e.
    
    * Revert "Remove vaccination infocard for now"
    
    This reverts commit ea593f35f845da3e245eb18c10abfb7099105410.

--- src/pages/StatsPage.vue
+++ src/pages/StatsPage.vue
@@ -71,7 +71,7 @@ export default {
         'hospitalizedCurrent',
         'icuCurrent',
         'deceasedToDate',
-        //'vaccinationSummary'
+        'vaccinationSummary'
       ]
     };
   },
