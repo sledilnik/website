b4897b26268e23fb395740403b158e6bcd02dbf3
Luka Renko <46861689+lukarenko@users.noreply.github.com>
2021-11-28 18:56:51 +0100

    Add FAQ

--- src/content/faq.js
+++ src/content/faq.js
@@ -42,6 +42,7 @@ export default [
             "patients-chart",
             "icu-patients-chart",
             "care-patients-chart",
+            "patients-age-chart",
             "hospitals-chart",
             "deceased-chart",
             "excess-deaths-chart",
