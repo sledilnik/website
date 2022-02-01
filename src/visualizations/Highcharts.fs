b095c6823efcb3262f4c7f8b93dc109827ab6bc1
Luka Renko <46861689+lukarenko@users.noreply.github.com>
2022-02-01 07:20:04 +0100

    New phase

--- src/visualizations/Highcharts.fs
+++ src/visualizations/Highcharts.fs
@@ -357,6 +357,7 @@ let basicChart
                     {| value=jsTime <| DateTime(2021,8,23); label=Some {| text=I18N.t "phase.18.description"; rotation=270; align="right"; x=12 |} |} |> pojo
                     {| value=jsTime <| DateTime(2021,11,8); label=Some {| text=I18N.t "phase.19.description"; rotation=270; align="right"; x=12 |} |} |> pojo
                     {| value=jsTime <| DateTime(2022,1,19); label=Some {| text=I18N.t "phase.20.description"; rotation=270; align="right"; x=12 |} |} |> pojo
+                    {| value=jsTime <| DateTime(2022,2,1);  label=Some {| text=I18N.t "phase.21.description"; rotation=270; align="right"; x=12 |} |} |> pojo
                 |]
                 plotBands=[|
                     {| ``from``=jsTime <| DateTime(2020,2,29);
@@ -455,9 +456,14 @@ let basicChart
                        label=Some {| align="center"; text=I18N.t "phase.19.title" |}
                     |}
                     {| ``from``=jsTime <| DateTime(2022,1,19);
-                       ``to``=jsTime <| DateTime.Today;
+                       ``to``=jsTime <| DateTime(2022,2,1);
                        color="transparent"
                        label=Some {| align="center"; text=I18N.t "phase.20.title" |}
+                    |}
+                    {| ``from``=jsTime <| DateTime(2022,2,1);
+                       ``to``=jsTime <| DateTime.Today;
+                       color="transparent"
+                       label=Some {| align="center"; text=I18N.t "phase.21.title" |}
                     |}
                     yield! shadedWeekendPlotBands
                 |]
