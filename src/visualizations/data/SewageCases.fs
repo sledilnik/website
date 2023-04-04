e2a41a78c911ad788712297e10e415814a047a00
Luka Renko <46861689+lukarenko@users.noreply.github.com>
2023-04-04 07:32:30 +0200

    Sewage cases and genomes data parsing

---
+++ src/visualizations/data/SewageCases.fs
@@ -0,0 +1,57 @@
+module Data.SewageCases
+
+open System
+open Fable.Core
+open Fable.SimpleHttp
+open Fable.SimpleJson
+
+let url = "https://api-stage.sledilnik.org/api/sewage-cases"
+
+type GeneMeasurement = {
+   raw: float
+   norm: float
+}
+
+type CasesEstimate = {
+   estimated: float
+   active100k: int
+}
+
+type SewageCases = {
+    year: int
+    month: int
+    day: int
+    station: string
+    flow: int
+    cod: int
+    n3: GeneMeasurement
+    cases: CasesEstimate
+    lat: float
+    lon: float
+    region: string
+    population: int
+    coverageRatio: float
+} with
+    member lt.Date = System.DateTime(lt.year, lt.month, lt.day)
+    member lt.JsDate12h = System.DateTime(lt.year, lt.month, lt.day) |> Highcharts.Helpers.jsTime12h
+
+
+let getOrFetch =
+    let fetch () = async {
+        let! response_code, json = Http.get url
+        return
+            match response_code with
+            | 200 ->
+                let data = json
+                           |> SimpleJson.parseNative
+                           |> Json.tryConvertFromJsonAs<SewageCases array>
+
+                match data with
+                | Error err ->
+                    Error (sprintf "Napaka pri nalaganju statističnih podatkov o čistilnih napravah: %s" err)
+                | Ok data ->
+                    Ok data
+            | _ ->
+                Error (sprintf "Napaka pri nalaganju statističnih podatkov o čistilnih napravah: %d" response_code)
+    }
+    Utils.memoize fetch
---
+++ src/visualizations/data/SewageGenomes.fs
@@ -0,0 +1,41 @@
+module Data.SewageGenomes
+
+open System
+open Fable.Core
+open Fable.SimpleHttp
+open Fable.SimpleJson
+
+let url = "https://api-stage.sledilnik.org/api/sewage-genomes"
+
+type SewageGenomes = {
+    year: int
+    month: int
+    day: int
+    station: string
+    genome: string
+    ratio: float
+    region: string
+} with
+    member lt.Date = System.DateTime(lt.year, lt.month, lt.day)
+    member lt.JsDate12h = System.DateTime(lt.year, lt.month, lt.day) |> Highcharts.Helpers.jsTime12h
+
+
+let getOrFetch =
+    let fetch () = async {
+        let! response_code, json = Http.get url
+        return
+            match response_code with
+            | 200 ->
+                let data = json
+                           |> SimpleJson.parseNative
+                           |> Json.tryConvertFromJsonAs<SewageGenomes array>
+
+                match data with
+                | Error err ->
+                    Error (sprintf "Napaka pri nalaganju statističnih podatkov o čistilnih napravah: %s" err)
+                | Ok data ->
+                    Ok data
+            | _ ->
+                Error (sprintf "Napaka pri nalaganju statističnih podatkov o čistilnih napravah: %d" response_code)
+    }
+    Utils.memoize fetch
